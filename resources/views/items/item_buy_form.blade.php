dfbf086aa8bedf58b1c4b561a6a8e3664794df84
Akinori Sakakibara <gorion.1213@gmail.com>
2021-12-04 10:55:51 +0900

    Try to purchase function

--- resources/views/items/item_buy_form.blade.php
+++ resources/views/items/item_buy_form.blade.php
@@ -43,7 +43,6 @@
 
             <div class="row mt-3 mb-3">
                 <div class="col-8 offset-2">
-                    {{-- <button class="btn btn-secondary btn-block">購入</button> --}}
                     <button class="btn btn-secondary btn-block" onclick="onSubmit(event)">購入</button>
                 </div>
             </div>
@@ -67,7 +66,7 @@
     expiryElement.mount('#expiry-form')
     cvcElement.mount('#cvc-form')
 
-    function submit(event) {
+    function onSubmit(event) {
         const msgDom = document.querySelector('.card-form-alert');
         msgDom.style.display = "none";
 
